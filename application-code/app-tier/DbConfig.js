ebcc0f32763cd47e5a1ea2b2e95852a7907e2712
nikita nand <105876777+Nikita130520@users.noreply.github.com>
2025-07-15 13:08:35 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-2.c3o2y2yuwa3o.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'admin123',
+    DB_DATABASE : 'webappdb'
+});
